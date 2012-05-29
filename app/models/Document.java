a6879a634722356608221da4ffa16a652fc3a430
Emmanuel Fontan <emmanuel.fontan@gmail.com>
2012-05-29 17:55:16 +0200

    Initialisation conf JPA sur entité Document

--- app/models/Document.java
+++ app/models/Document.java
@@ -1,5 +1,12 @@
 package models;
 
-public class Document {
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import play.db.jpa.Model;
+
+@Entity
+@Table(name = "tbl_document")
+public class Document extends Model {
 
 }
